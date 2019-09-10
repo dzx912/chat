8838bd4eba511925fff0a62397d1d60873925226
Alexander Murashov <alex.v.murashov@gmail.com>
2019-09-11 00:09:05 +0300

    Introduced new REST services: GET /images to list all images; GET /images/<imageId> to download given image; and POST /images to upload new image.

--- src/main/java/org/example/verticle/MongoDbVerticle.java
+++ src/main/java/org/example/verticle/MongoDbVerticle.java
@@ -5,10 +5,14 @@ import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoClient;
 
 import java.util.List;
+
+import static org.example.responses.Responses.error;
+import static org.example.responses.Responses.ok;
 
 public class MongoDbVerticle extends AbstractVerticle {
     private MongoClient client;
@@ -17,6 +21,9 @@ public class MongoDbVerticle extends AbstractVerticle {
         client = MongoClient.createShared(vertx, new JsonObject().put("db_name", "my_DB"));
         vertx.eventBus().consumer("db.message.save", this::messageSave);
         vertx.eventBus().consumer("db.message.history", this::messageHistory);
+        vertx.eventBus().consumer("db.images.get", this::imagesGet);
+        vertx.eventBus().consumer("db.images.getById", this::imagesGetById);
+        vertx.eventBus().consumer("db.images.save", this::imagesSave);
     }
 
     private void messageHistory(Message<String> message) {
@@ -38,5 +45,47 @@ public class MongoDbVerticle extends AbstractVerticle {
         } else {
             System.out.println("ERROR MongoDB: " + result.cause());
         }
+    }
+
+    private void imagesGet(Message<Void> message) {
+        client.find("images", new JsonObject(), async -> {
+            JsonObject result;
+
+            if (async.failed()) {
+                result = error(async.cause());
+
+            } else {
+                JsonArray items = new JsonArray();
+                async.result().forEach(items::add);
+                result = ok(items);
+            }
+
+            message.reply(result);
+        });
+    }
+
+    private void imagesGetById(Message<String> message) {
+        JsonObject query = new JsonObject().put("_id", message.body());
+
+        client.find("images", query, async -> {
+            JsonObject result;
+
+            if (async.failed()) {
+                result = error(async.cause());
+
+            } else {
+                // NB: search by _id returns no more than 1 document
+                JsonObject item = async.result().stream().findAny().orElse(null);
+                result = ok(item);
+            }
+
+            message.reply(result);
+        });
+    }
+
+    private void imagesSave(Message<JsonObject> message) {
+        JsonObject image = message.body();
+
+        client.insert("images", image, async -> message.reply(async.failed() ? error(async.cause()) : ok(image)));
     }
 }
